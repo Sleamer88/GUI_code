4f443956dc679146059607d600cfc6bb0d45aead
Sleamer88 <119081071+Sleamer88@users.noreply.github.com>
2023-05-15 13:06:39 +0200

    Update Bubbles_page.py

--- Bubbles_page.py
+++ Bubbles_page.py
@@ -42,12 +42,18 @@ c_escape = circle_canvas.create_oval(114,89,214,189)
 c_cruise = circle_canvas.create_oval(4,199,64,259)
 c_party = circle_canvas.create_oval(64,189,204,329)
 
+import subprocess
+
+def run_program():
+    subprocess.call(["python", "Country_selection(proof of concept).py"])
+
+
 t_beach = circle_canvas.create_text(34,59, text="Beach")
 t_historical = circle_canvas.create_text(114,54, text="Historical")
 t_city = circle_canvas.create_text(184,49, text="City")
 t_adventurous = circle_canvas.create_text(59,144, text="Adventurous")
 t_escape = circle_canvas.create_text(164,139, text="Escape")
 t_cruise = circle_canvas.create_text(34,229, text="cruise")
-t_party = Button(circle_canvas, text="Party/Clubbing", command=exit_program).grid(padx=94, pady=249)
+t_party = Button(circle_canvas, text="Party/Clubbing", command=run_program).grid(padx=94, pady=249)
 
 root.mainloop()
