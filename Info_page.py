ecd7a92d257686cdd9c18bc63fd934114cfb80ad
Sleamer88 <119081071+Sleamer88@users.noreply.github.com>
2023-05-15 14:14:38 +0200

    Add files via upload

---
+++ Info_page.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from datetime import datetime
+
+
+def logaction(logtext):
+    delta = datetime.now() - startdatetime
+    logfile.write(str(delta) + "" + logtext + "\n")
+
+
+startdatetime = datetime.now()
+starttime = datetime.timestamp(startdatetime)
+logfile = open("logging_prototype.txt", "a")
+logaction("New user tracking session @" + str(startdatetime))
+
+root = Tk()
+root.title("Frame for window")
+root.geometry("260x300")
+root.config(bg="white")
+
+left_frame = Frame(root, width=200, height=400)
+left_frame.grid(row=0, column=0, padx=10, pady=5)
+
+tool_bar = Frame(left_frame, width=180, height=185, bg="grey")
+tool_bar.grid(row=2, column=0, padx=5, pady=5)
+
+Label(left_frame, text="Account overview").grid(row=1, column=0, padx=5)
+
+Label(tool_bar, text="Name: AIR Amsterdam").grid(row=0, column=0, padx=5, pady=5)
+Label(tool_bar, text="Address: Amstelstraat 24 \n 1017 DA Amsterdam").grid(row=2, column=0, padx=5, pady=5)
+Label(tool_bar, text="Phone: 020 820 0670").grid(row=3, column=0, padx=5, pady=5)
+Label(tool_bar, text="Opening hours: Mon-Wed: closed \n Thurs-Sun: 22:00 - 05:00").grid(row=4, column=0, padx=5, pady=5)
+Label(tool_bar, text="Service options: Dine in \n no takeaway, no delivery").grid(row=5, column=0, padx=5, pady=5)
+
+
+import subprocess
+
+def run_program():
+    subprocess.call(["python", "Questionnaire(after_experience).py"])
+
+
+btn = Button(root, text='Next', command=run_program)
+btn.grid()
+
+root.mainloop()
---
+++ Rbubbles.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from tkinter import ttk
+from datetime import datetime
+
+def logaction(logtext):
+    delta = datetime.now() - startdatetime
+    logfile.write(str(delta) + "" + logtext + "\n")
+
+
+startdatetime = datetime.now()
+starttime = datetime.timestamp(startdatetime)
+logfile = open("logging_prototype.txt", "a")
+logaction("New user tracking session @" + str(startdatetime))
+
+
+root = Tk()
+root.title("Welcome")
+
+
+#Define the geometry of window
+root.geometry("250x400")
+
+def exit_program():
+   root.destroy()
+
+#Title frame
+title_frame = Frame(root, width=300, height=100)
+title_frame.grid(row=0, sticky="wens")
+
+Label(title_frame, text="Here are some revised options \n for your next holiday:").grid(row=1, column=0, padx=20, pady=10, sticky="wens")
+
+#Create a canvas object
+circle_canvas = Canvas(root,width=500, height=400)
+circle_canvas.grid(row=2, rowspan= 10, sticky="n")
+
+#Draw an Oval in the canvas
+
+c_historical = circle_canvas.create_oval(64,4,164,104)
+c_adventurous = circle_canvas.create_oval(4,89,114,199)
+c_escape = circle_canvas.create_oval(114,89,214,189)
+c_party = circle_canvas.create_oval(64,189,204,329)
+
+t_historical = circle_canvas.create_text(114,54, text="Historical")
+t_adventurous = circle_canvas.create_text(59,144, text="Adventurous")
+t_escape = circle_canvas.create_text(164,139, text="Escape")
+t_party = Button(circle_canvas, text="Party/Clubbing", command=root.destroy).grid(padx=94, pady=249)
+
+root.mainloop()
