bdfa2668d881ef0f5112a939fb2ee50a9e9c604f
Sleamer88 <119081071+Sleamer88@users.noreply.github.com>
2023-05-15 14:13:16 +0200

    Update Country_selection(proof of concept).py

--- Country_selection(proof of concept).py
+++ Country_selection(proof of concept).py
@@ -1,22 +1,59 @@
 from tkinter import *
 from tkinter import ttk
 
+
 root = Tk()
-root.geometry("200x150")
+root.geometry("300x580")
+
+category = Canvas(root,width=300, height=100)
+category.grid(row=0, rowspan= 2, sticky="n")
+o_category = category.create_oval(2,2,298,48)
+t_category = category.create_text(148,23, text="Party/Clubbing")
 
 frame = Frame(root)
-frame.pack()
+frame.grid()
+
+Label(frame, text="Where would you like to visit?").grid(row=1, column=0, padx=5)
 
 vlist = ["Netherlands"]
 xlist = ["Amsterdam"]
 
+
+
 Combov = ttk.Combobox(frame, values= vlist)
 Combov.set("Choose a country")
-Combov.pack(padx=5, pady=5)
+Combov.grid(padx=5, pady=5)
 
 Combox = ttk.Combobox(frame,values= xlist)
 Combox.set("Choose a city")
-Combox.pack(padx=6, pady=6)
+Combox.grid(padx=6, pady=6)
+
+# Import Required Library
+from tkcalendar import Calendar
+
+Label(frame, text="When would you like to visit?").grid(row=5, column=0, padx=5)
+
+cal = Calendar(root, selectmode='day',
+               year=2020, month=5,
+               day=22)
+
+cal.grid(pady=20)
+
+
+def grad_date():
+    date.config(text="Selected Date at Amsterdam is: " + cal.get_date())
+
+
+# Add Button and Label
+Button(root, text="Get Date",
+       command=grad_date).grid(pady=20)
+
+date = Label(root, text="")
+date.grid(pady=20)
+
+
+Combov.grid(padx=5, pady=5)
+
 
 import subprocess
 
@@ -25,6 +62,6 @@ def run_program():
 
 
 btn = Button(root, text='Next', command=run_program)
-btn.pack()
+btn.grid()
 
 root.mainloop()
