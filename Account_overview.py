94f4bed4d78ad88f55b02c61b64780e54bccfc83
Sleamer88 <119081071+Sleamer88@users.noreply.github.com>
2023-05-15 21:14:55 +0200

    Update Account_overview.py

--- Account_overview.py
+++ Account_overview.py
@@ -10,35 +10,35 @@ def logaction(logtext):
 startdatetime = datetime.now()
 starttime = datetime.timestamp(startdatetime)
 logfile = open("logging_prototype.txt", "a")
-logaction("Profile opened @" + str(startdatetime))
+logaction("New user tracking session @" + str(startdatetime))
 
 root = Tk()
-root.title("Profile")
-root.geometry("300x500")
+root.title("Frame for window")
+root.geometry("150x250")
+root.config(bg="white")
 
-title_frame = Frame(root, width=200, height=400, pady=10)
-title_frame.pack()
-Label(title_frame, text="Account overview").pack()
+left_frame = Frame(root, width=200, height=400)
+left_frame.grid(row=0, column=0, padx=10, pady=5)
 
+tool_bar = Frame(left_frame, width=180, height=185, bg="grey")
+tool_bar.grid(row=2, column=0, padx=5, pady=5)
 
-tool_bar = Frame(title_frame, width=180, height=185, bg="grey", pady=10)
-tool_bar.pack()
+Label(left_frame, text="Account overview").grid(row=1, column=0, padx=5)
 
-Label(tool_bar, text="Name: Dan ").pack()
-Label(tool_bar, text="Email: dantheman@gmail.com ").pack()
-Label(tool_bar, text="Gender: Male ").pack()
-Label(tool_bar, text="Occupation: Construction worker ").pack()
-Label(tool_bar, text="Age: 38 ").pack()
+Label(tool_bar, text="Name: Dan").grid(row=0, column=0, padx=5, pady=5)
+Label(tool_bar, text="Email: dantheman@gmail.com").grid(row=2, column=0, padx=5, pady=5)
+Label(tool_bar, text="Gender: Male").grid(row=3, column=0, padx=5, pady=5)
+Label(tool_bar, text="Occupation: Construction Worker").grid(row=4, column=0, padx=5, pady=5)
+Label(tool_bar, text="Age: 38").grid(row=5, column=0, padx=5, pady=5)
 
 
 import subprocess
 
 def run_program():
-    root.destroy()
-    subprocess.call(["python", "Country_selection(proof of concept).py"])
+    subprocess.call(["python", "Country_selection.py"])
 
 
 btn = Button(root, text='Next', command=run_program)
-btn.pack()
+btn.grid()
 
 root.mainloop()
