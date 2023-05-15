cfb862aad5297fc819d9756660f2698dbdd87c66
Sleamer88 <119081071+Sleamer88@users.noreply.github.com>
2023-05-15 13:05:50 +0200

    Update Title screen of app.py

--- Title screen of app.py
+++ Title screen of app.py
@@ -11,44 +11,20 @@ starttime = datetime.timestamp(startdatetime)
 logfile = open("logging_prototype.txt", "a")
 logaction("New user tracking session @" + str(startdatetime))
 
-# root = Tk()
-# root.title("Welcome")
-#
-#
-# #Define the geometry of window
-# root.geometry("1000x4000")
-#
-# #Create a canvas object
-# c= Canvas(root,width=400, height=400)
-# c.pack()
-#
-# #Draw an Oval in the canvas
-# c.create_oval(60,60,210,210)
-
-
-# from tkinter import PhotoImage
-#
-# image = PhotoImage(file="test.png")
-#
-# root = Tk()
-# cv = Canvas(root)
-# cv.create_image(10,10, image=image)
-# cv.pack()
-# root.mainloop()
-
 
 #Import the required Libraries
 from tkinter import *
 from PIL import Image,ImageTk
 
 #Create an instance of tkinter frame
-win = Tk()
+root = Tk()
 
 #Set the geometry of tkinter frame
-win.geometry("750x650")
+root.geometry("450x250")
 
 #Create a canvas
-canvas= Canvas(win, width= 600, height= 400)
+canvas= Canvas(root, width= 400, height= 200)
+canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
 canvas.pack()
 
 #Load an image in the script
@@ -57,6 +33,25 @@ img= ImageTk.PhotoImage(Image.open("Logo.png"))
 #Add image to the Canvas Items
 canvas.create_image(10,10,anchor=NW,image=img)
 
-win.mainloop()
+import subprocess
+
+def run_program():
+    subprocess.call(["python", "Hi-fi (questionnaire).py"])
+
+
+btn = Button(root, text='Questionnaire', command=run_program)
+btn.pack()
+
+import subprocess
+
+def run_program():
+    subprocess.call(["python", "Bubbles_page.py"])
+
+
+btn = Button(root, text='Fast track (less accurate)')
+btn.pack()
+
+
+root.mainloop()
 
 
